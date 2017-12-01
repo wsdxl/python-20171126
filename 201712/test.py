bf68b6189b5726df30d4f08c79b9385516c9686e
wsdxl <506615839@qq.com>
2017-12-01 16:54:51 +0800

    爬百度首页数据-20171201

---
+++ 201712/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib.request
+import os
+import webbrowser
+#网址
+url = "http://www.baidu.com"
+#请求
+request = urllib.request.Request(url)
+#爬取结果
+response = urllib.request.urlopen(request)
+#读取结果
+data = response.read()
+#设置解码方式
+data = data.decode('utf-8')
+#获取绝对路径
+path = os.path.abspath('')
+dirpath = os.path.join(path, 'spider')
+if os.path.isdir(dirpath):
+    print('exist')
+else:
+    print('no exist')
+    os.mkdir(dirpath)
+filepath = os.path.join(dirpath, 'test1.html')
+file = open(filepath, 'w', encoding='utf-8')
+#写入数据
+file.write(data)
